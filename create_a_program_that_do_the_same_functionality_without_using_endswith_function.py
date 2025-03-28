b59f3d7084bfeef64be9d87891efc15b59a0cbf7
talamor-shella <shellatalamor@gmail.com>
2025-03-29 04:02:59 +0800

    AddedPseudocode

--- create_a_program_that_do_the_same_functionality_without_using_endswith_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_endswith_function.py
@@ -1,1 +1,6 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+#ask user input
+#ask user input the suffix
+#if statement 
+#print true or false
