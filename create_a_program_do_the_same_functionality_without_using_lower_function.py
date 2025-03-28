a0e24b5d7880b67a4d859838686afea779fbd0a6
talamor-shella <shellatalamor@gmail.com>
2025-03-28 22:05:10 +0800

    AddedPseudocode

--- create_a_program_do_the_same_functionality_without_using_lower_function.py
+++ create_a_program_do_the_same_functionality_without_using_lower_function.py
@@ -1,2 +1,7 @@
 #Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
+#ask user input 
+#for loop through each character
+#if statement to check if uppercase
+#converts to lowercase if uppercase
+#prints the user input in lower casee
