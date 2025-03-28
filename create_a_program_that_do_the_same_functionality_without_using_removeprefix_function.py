a73af0356f41e616f9c436da3f4173815597abd7
talamor-shella <shellatalamor@gmail.com>
2025-03-28 15:33:27 +0800

    ask user input a word

--- create_a_program_that_do_the_same_functionality_without_using_removeprefix_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_removeprefix_function.py
@@ -1,6 +1,8 @@
 #Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 
 #ask user input a word
+word = input("Enter a word: ")
+
 #ask user input a characters they want to remove
 #if statement
 #prints the word without the removed characters
