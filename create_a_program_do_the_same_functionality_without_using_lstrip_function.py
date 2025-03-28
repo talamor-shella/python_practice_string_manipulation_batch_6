957b2ac1eea0fcbfc5c4d9923b148e6be079237a
talamor-shella <shellatalamor@gmail.com>
2025-03-28 14:30:06 +0800

    ask user input

--- create_a_program_do_the_same_functionality_without_using_lstrip_function.py
+++ create_a_program_do_the_same_functionality_without_using_lstrip_function.py
@@ -1,6 +1,8 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 #ask user input 
+word = input("Enter a word: ")
+
 #initialize a variable to zero
 #while loop 
 #prints the remove space characters
