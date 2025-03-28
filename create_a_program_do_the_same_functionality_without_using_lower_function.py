63d655c61e702456170c9aa0e6e27f1e408d2b4d
talamor-shella <shellatalamor@gmail.com>
2025-03-28 22:54:56 +0800

    Fixed Error in for loop and if-else statement and prints the word in lowercase but there is an error in displaying the elements

--- create_a_program_do_the_same_functionality_without_using_lower_function.py
+++ create_a_program_do_the_same_functionality_without_using_lower_function.py
@@ -1,19 +1,24 @@
 #Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 #ask user input 
-word = input("Enter a word: ")
+word = str(input("Enter a word: "))
+
+#make a new list
+new_word = []
 
 #for loop through each character
 for char in word:
     
     #if statement to check if uppercase
-    if 65 <= ord(word) <= 90:
+    if 65 <= ord(char) <= 90:
 
         #converts to lowercase if uppercase
-        lowercase_letter = chr(ord(word) + 32)
+        lowercase_letter = chr(ord(char) + 32)
 
     else:
-        lowercase_letter = word #keep if the same
+        lowercase_letter = char #keep if the same
+
+    new_word.append(lowercase_letter)    
 
 #prints the user input in lower case
-print(lowercase_letter)
+print(new_word)
