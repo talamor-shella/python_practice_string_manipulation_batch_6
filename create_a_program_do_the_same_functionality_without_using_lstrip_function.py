2812db3e64cc33bd474c209c07e25644f944b263
talamor-shella <shellatalamor@gmail.com>
2025-03-28 15:04:34 +0800

    FinishedCode: fixed an error by creating new variable slicing the string to remove spaces and prints the remove space characters

--- create_a_program_do_the_same_functionality_without_using_lstrip_function.py
+++ create_a_program_do_the_same_functionality_without_using_lstrip_function.py
@@ -1,15 +1,19 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 #ask user input 
-word = input("Enter a word: ")
+word = str(input("Enter a word: "))
 
 #initialize a variable to zero
 spaces = 0
 
 #while loop 
-while spaces < len(word) and word(spaces) == " ":
+while spaces < len(word) and word == " ":
 
     #increment spaces by 1
     spaces += 1
 
+#slicing the string to remove spaces
+without_spaces = word[spaces:]
+
 #prints the remove space characters
+print (repr(without_spaces))
