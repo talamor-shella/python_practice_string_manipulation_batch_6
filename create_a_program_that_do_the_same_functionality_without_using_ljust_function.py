84316160da48599372edbfe01533c17c94804b25
talamor-shella <shellatalamor@gmail.com>
2025-03-29 05:46:18 +0800

    AddedPseudocode

--- create_a_program_that_do_the_same_functionality_without_using_ljust_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_ljust_function.py
@@ -1,1 +1,6 @@
 #Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+#ask user input a word
+#ask user how many spaces they want to add at the end
+#if-else statement
+#print the word with spaces added
