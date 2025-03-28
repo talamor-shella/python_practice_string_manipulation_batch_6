33669c8889893cdce3efc76f7ab4cee76c56b293
talamor-shella <shellatalamor@gmail.com>
2025-03-29 06:20:03 +0800

    ask user input a word and number of spaces they want to add

--- create_a_program_that_do_the_same_functionality_without_using_ljust_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_ljust_function.py
@@ -1,6 +1,10 @@
 #Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 #ask user input a word
+word = str(input("Enter a word: "))
+
 #ask user how many spaces they want to add at the end
+spaces = int(input("Enter a number of spaces you want to add at end: "))
+
 #if-else statement
 #print the word with spaces added
