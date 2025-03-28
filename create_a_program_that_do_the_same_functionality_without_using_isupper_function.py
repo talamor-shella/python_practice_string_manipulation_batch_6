44cfcd3a669fbeeaf39d74058722882c1c9a879d
talamor-shella <shellatalamor@gmail.com>
2025-03-29 01:56:25 +0800

    Error for loop

--- create_a_program_that_do_the_same_functionality_without_using_isupper_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_isupper_function.py
@@ -1,6 +1,20 @@
 #Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 #ask user input
+word = input("Enter a word: ")
+
 #use for loop 
-# if-else statement 
-#prints true or false 
+for char in word:
+
+    # if-else statement 
+    if 65 <= ord(char) <= 90:
+        #prints true if all characters is uppercase
+        print("True") 
+
+    else:
+        #prints true if all characters is uppercase
+        print("False")
+    
+
+
+
