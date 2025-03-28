d11d1c73d613b62e3d9c75b53d3b08265d773948
talamor-shella <shellatalamor@gmail.com>
2025-03-28 22:38:38 +0800

    looping through each character and if-else statement to check if uppercase or lowercase

--- create_a_program_do_the_same_functionality_without_using_lower_function.py
+++ create_a_program_do_the_same_functionality_without_using_lower_function.py
@@ -4,6 +4,16 @@
 word = input("Enter a word: ")
 
 #for loop through each character
-#if statement to check if uppercase
-#converts to lowercase if uppercase
-#prints the user input in lower casee
+for char in word:
+    
+    #if statement to check if uppercase
+    if 65 <= ord(word) <= 90:
+
+        #converts to lowercase if uppercase
+        lowercase_letter = chr(ord(word) + 32)
+
+    else:
+        lowercase_letter = word #keep if the same
+
+#prints the user input in lower case
+print(word)
