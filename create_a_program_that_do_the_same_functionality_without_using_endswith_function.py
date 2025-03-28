087a21eb90274a68663702bf54587c0b544822bb
talamor-shella <shellatalamor@gmail.com>
2025-03-29 05:28:09 +0800

    ask user to input a word

--- create_a_program_that_do_the_same_functionality_without_using_endswith_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_endswith_function.py
@@ -1,6 +1,8 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 #ask user input
+word = input("Enter a word: ")
+
 #ask user input the suffix
 #if statement 
 #print true or false
