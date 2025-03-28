e39ae15447926b241a85d1c357680c78a7dac297
talamor-shella <shellatalamor@gmail.com>
2025-03-29 05:40:48 +0800

    FileCreated: create_a_program_that_do_the_same_functionality_without_using_ljust_function

---
+++ create_a_program_that_do_the_same_functionality_without_using_ljust_function.py
@@ -0,0 +1,1 @@
+#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
