61aaf2f9ef72e6a804f1064332e0656342bc1bf9
talamor-shella <shellatalamor@gmail.com>
2025-03-29 16:33:42 +0800

    ask user input a string

--- create_a_program_that_do_the_same_functionality_without_using_swapcase_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_swapcase_function.py
@@ -1,6 +1,9 @@
 #Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 #ask user input a string
+string = str(input("Enter a string: "))
+
 #looping to the string
+
 #if-else statement
 #prints the string without using swapcase
