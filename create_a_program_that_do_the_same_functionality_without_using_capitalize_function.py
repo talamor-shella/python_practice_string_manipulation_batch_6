0e732055fba02e16d0a27be523ac023919c80dd0
talamor-shella <shellatalamor@gmail.com>
2025-03-29 17:06:41 +0800

    FileCreated: create_a_program_that_do_the_same_functionality_without_using_capitalize_function

---
+++ create_a_program_that_do_the_same_functionality_without_using_capitalize_function.py
@@ -0,0 +1,1 @@
+#Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
