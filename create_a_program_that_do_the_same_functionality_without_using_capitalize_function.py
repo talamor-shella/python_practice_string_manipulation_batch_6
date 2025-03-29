c839222caaf33e8823759e0b30dda1ec54f89eb8
talamor-shella <shellatalamor@gmail.com>
2025-03-30 00:54:51 +0800

    fixed errors in if-else statement

--- create_a_program_that_do_the_same_functionality_without_using_capitalize_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_capitalize_function.py
@@ -3,19 +3,11 @@
 #ask user input a string
 string = str(input("Enter a string: "))
 
-#assign the first index
-first_letter = string[0]
 
-#for loop 
-for char in string:
-
-    #if-else statement
-    if 'a' <= first_letter <= 'z':
-        swapcase_letter = chr(ord(char) - 32) #converts lower case to uppercase
-    else:
-        swapcase_letter = char
-    
-
+#if-else statement
+if string: 
+    capitalize_string = string[0].upper() + string[1:].lower()
+else:
+    capitalize_string = ""
 #print the string without using capitalize function
-
 
