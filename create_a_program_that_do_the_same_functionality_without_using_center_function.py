5217df144257eb04b213078c20a98878c72c5f3f
talamor-shella <shellatalamor@gmail.com>
2025-03-29 15:58:47 +0800

    AddedPseudocode

--- create_a_program_that_do_the_same_functionality_without_using_center_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_center_function.py
@@ -1,1 +1,7 @@
 #Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+#ask user input a string
+#ask user input a number for the width of characters
+#variable that finds the total spaces need
+#split the spaces
+#print the result
