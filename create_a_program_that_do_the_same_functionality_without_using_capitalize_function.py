a48e468343a8fe7ee765ddb76718a1ffc95ab0cb
talamor-shella <shellatalamor@gmail.com>
2025-03-29 17:10:26 +0800

    ask user to input a string

--- create_a_program_that_do_the_same_functionality_without_using_capitalize_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_capitalize_function.py
@@ -1,6 +1,8 @@
 #Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 #ask user input a string
+string = str(input("Enter a string: "))
+
 #assign the first index
 #if else
 #print the string without using capitalize function
