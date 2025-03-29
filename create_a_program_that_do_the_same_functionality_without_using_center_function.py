dd02831778d33a1feab571be28223276fbd889c0
talamor-shella <shellatalamor@gmail.com>
2025-03-29 16:04:11 +0800

    ask user input string and numbere for total width

--- create_a_program_that_do_the_same_functionality_without_using_center_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_center_function.py
@@ -1,7 +1,11 @@
 #Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 #ask user input a string
+string = str(input("input a string: "))
+
 #ask user input a number for the width of characters
+total_width = int(input("Enter a number for total width: "))
+
 #variable that finds the total spaces need
 #split the spaces
 #print the result
