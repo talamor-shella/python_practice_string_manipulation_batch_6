c06dc46e85ed53a1863bf098cd604faad72d491f
talamor-shella <shellatalamor@gmail.com>
2025-03-30 18:43:22 +0800

    ask user input and use split function

--- create_a_program_that_do_the_same_functionality_without_using_title_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_title_function.py
@@ -1,6 +1,8 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
-#ask user input a string
+#ask user input a string and use a split method
+words = str(input("Enter a string: ")).split()
+
 #loop to the string
 #if else
 #create a new list
