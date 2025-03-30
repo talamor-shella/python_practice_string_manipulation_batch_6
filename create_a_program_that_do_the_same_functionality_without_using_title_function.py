d0491204c488b3bd258f41851ed793f6094be54a
talamor-shella <shellatalamor@gmail.com>
2025-03-30 18:41:28 +0800

    AddedPseuodocode

--- create_a_program_that_do_the_same_functionality_without_using_title_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_title_function.py
@@ -1,3 +1,7 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
-
+#ask user input a string
+#loop to the string
+#if else
+#create a new list
+#print the result without using title() function
