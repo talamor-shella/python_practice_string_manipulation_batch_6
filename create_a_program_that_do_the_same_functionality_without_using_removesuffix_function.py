6298fcd7e503c49c20aa5f0544b9faf3f1c50336
talamor-shella <shellatalamor@gmail.com>
2025-04-01 03:58:46 +0800

    ask user input a word and a suffix they wnat to remove

--- create_a_program_that_do_the_same_functionality_without_using_removesuffix_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_removesuffix_function.py
@@ -1,6 +1,10 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 #Ask user input
+string = input("Enter a word: ")
+
 #Ask user they want to remove
+character = input("Enter a suffix you want to remove: ")
+
 #if-else statement
 #prints the characters without the remove suffix
