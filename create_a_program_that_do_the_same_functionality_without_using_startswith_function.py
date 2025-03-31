32201b1f168aad97319c0af737bbb31c23de47ee
talamor-shella <shellatalamor@gmail.com>
2025-04-01 04:59:16 +0800

    ask user input a word and a prefix

--- create_a_program_that_do_the_same_functionality_without_using_startswith_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_startswith_function.py
@@ -1,7 +1,11 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 #ask user input a word or sentence
+word = input("Enter a word or a sentence: ")
+
 #ask user input starting with 
+prefix = input("Enter a prefix in the word: ")
+
 #if statement
 #returns true and false
 #prints true or false
