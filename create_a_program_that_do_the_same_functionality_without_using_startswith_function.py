25ad49f8f11cd2a0b00bfeae41e7aecdac4362ff
talamor-shella <shellatalamor@gmail.com>
2025-04-01 05:12:30 +0800

    if statement to compare the beginning of word with prefix and else statement if not same

--- create_a_program_that_do_the_same_functionality_without_using_startswith_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_startswith_function.py
@@ -1,11 +1,19 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 #ask user input a word or sentence
-word = input("Enter a word or a sentence: ")
+word = input("Enter a word or a sentence: ").lower()
 
 #ask user input starting with 
-prefix = input("Enter a prefix in the word: ")
+prefix = input("Enter a prefix in the word: ").lower()
+#if-else statement
+if word [:len(prefix)] == prefix:
 
-#if statement
-#returns true and false
+    #returns true
+    is_prefix = True
+
+else:
+    
+    #returns false
+    is_prefix = False
+
 #prints true or false
