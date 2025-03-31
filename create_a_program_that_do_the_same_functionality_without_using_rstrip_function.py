2944d159971a7bb41d4aa78973734ca7c70ab7cc
talamor-shella <shellatalamor@gmail.com>
2025-04-01 03:23:40 +0800

    addedpseudocode

--- create_a_program_that_do_the_same_functionality_without_using_rstrip_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_rstrip_function.py
@@ -1,1 +1,6 @@
 #Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+#ask user input 
+#variable finding the last non-space character
+#for loop
+#if statement to find non space character
