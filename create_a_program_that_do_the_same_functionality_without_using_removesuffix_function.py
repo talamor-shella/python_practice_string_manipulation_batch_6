7ec43223047024bb6bdedb34a3655dcad3b9b06a
talamor-shella <shellatalamor@gmail.com>
2025-04-01 03:56:57 +0800

    FileCreated: create_a_program_that_do_the_same_functionality_without_using_removesuffix_function

---
+++ create_a_program_that_do_the_same_functionality_without_using_removesuffix_function.py
@@ -0,0 +1,6 @@
+#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
+#Ask user input
+#Ask user they want to remove
+#if-else statement
+#prints the characters without the remove suffix
