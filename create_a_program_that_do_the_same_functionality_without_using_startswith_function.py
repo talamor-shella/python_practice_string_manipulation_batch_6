f61f43d9ba4762d7f00dcb6be737cd2d762ee18f
talamor-shella <shellatalamor@gmail.com>
2025-04-01 04:55:33 +0800

    addedpseudocode

--- create_a_program_that_do_the_same_functionality_without_using_startswith_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_startswith_function.py
@@ -1,1 +1,7 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+#ask user input a word or sentence
+#ask user input starting with 
+#if statement
+#returns true and false
+#prints true or false
