e62273a1feef2e0413eab97f97e60acdfd830b25
talamor-shella <shellatalamor@gmail.com>
2025-04-01 04:31:47 +0800

    addedpseudocode

--- create_a_program_that_do_the_same_functionality_without_using_upper_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_upper_function.py
@@ -1,1 +1,7 @@
 #Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+#ask user input 
+#for loop through string
+#if statement for ascii values
+#convert to upper if lower
+#prints all characters upper case
