5415bdf480342a7cd68f37f2932b4459bdd87303
talamor-shella <shellatalamor@gmail.com>
2025-04-01 14:29:22 +0800

    ask user input a word and a number for total length

--- create_a_program_that_do_the_same_functionality_without_using_rjust_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_rjust_function.py
@@ -1,7 +1,11 @@
 #Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 #ask user input a word
+string = input("Enter a word: ")
+
 #ask user total length 
+total_length = int("Enter a number for total length: ")
+
 #find how many spaces need
 #if-else statement
 #print the word with spaces 
