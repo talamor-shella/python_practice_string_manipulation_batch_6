f3c6951adefe2410db3434ee4f28d3c88d59cf48
talamor-shella <shellatalamor@gmail.com>
2025-04-01 17:59:10 +0800

    FinishedCode: fixed issue in for loop, prints the index

--- create_a_program_that_do_the_same_functionality_without_using_index_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_index_function.py
@@ -1,16 +1,16 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 #ask user input a string
-string = input("Enter a word: ")
+string = input("Enter a word: ").lower()
 
 #ask user input what they want to find the index
-character = input("Enter a character you want to find the index: ")
-
+character = input("Enter a character you want to find the index: ").lower()
 #for loop
-for i in range(len(string) - len(character)):
+for i in range(len(string) - len(character) + 1):
 
     #if statement 
     if string[i:i+len(character)] == character:
 
         #prints the index 
         print(i)
+        break
