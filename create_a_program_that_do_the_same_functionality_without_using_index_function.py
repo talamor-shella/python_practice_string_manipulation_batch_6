8d2db3a509bcc17dffb0c89b44b97b6715d4fee3
talamor-shella <shellatalamor@gmail.com>
2025-04-01 17:40:09 +0800

    ask user input string and what they want to find index

--- create_a_program_that_do_the_same_functionality_without_using_index_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_index_function.py
@@ -1,7 +1,11 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 #ask user input a string
+string = input("Enter a word: ")
+
 #ask user input what they want to find the index
+character = input("Enter a character you want to find the index: ")
+
 #for loop
 #if statement 
 #prints the index 
