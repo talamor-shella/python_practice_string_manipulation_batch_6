86fa71cf0b1f832f3c8cfda5d6794061c051dceb
talamor-shella <shellatalamor@gmail.com>
2025-04-01 14:55:51 +0800

    finds total space needed for adding zeros

--- create_a_program_that_do_the_same_functionality_without_using_zfill_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_zfill_function.py
@@ -1,11 +1,13 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function
 
 #ask user input a number
-number = int(input("Enter a number: "))
+number = (input("Enter a number: "))
 
 #ask user specified length
 specified_length = int(input("Enter a number for specified length: "))
 
 #find total space needed to add zeros
+total_space = specified_length - len(number)
+
 #if statement using rjust function and else statement
 #prints the complete number of characters
