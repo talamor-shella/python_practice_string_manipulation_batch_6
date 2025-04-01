70ce6a5b388d847012ae688a028b95ec9821dd34
talamor-shella <shellatalamor@gmail.com>
2025-04-01 15:52:57 +0800

    ask user input a word and what they want to count

--- create_a_program_that_do_the_same_functionality_without_using_count_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_count_function.py
@@ -1,7 +1,11 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 #ask user input word or sentence
+string = input("Enter any word or sentence: ").lower()
+
 #ask user input a character they want to count
+character = input("Enter a letter or word you want to count: ").lower()
+
 #initialize a counter
 #for loop
 #if-else statement
