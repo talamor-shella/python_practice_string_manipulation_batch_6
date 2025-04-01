2d974eacf25a662a93dd29a5f03d1af33638e32f
talamor-shella <shellatalamor@gmail.com>
2025-04-01 17:31:06 +0800

    addedpseudocode

--- create_a_program_that_do_the_same_functionality_without_using_index_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_index_function.py
@@ -1,1 +1,7 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+#ask user input a string
+#ask user input what they want to find the index
+#for loop
+#if statement 
+#prints the index 
