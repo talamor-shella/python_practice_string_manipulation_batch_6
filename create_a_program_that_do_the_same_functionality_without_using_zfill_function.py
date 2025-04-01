6956e82457320c16beae90aad6b280ac16bfc647
talamor-shella <shellatalamor@gmail.com>
2025-04-01 14:53:28 +0800

    ask user input a number and specified length

--- create_a_program_that_do_the_same_functionality_without_using_zfill_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_zfill_function.py
@@ -1,1 +1,11 @@
-#Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+#Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function
+
+#ask user input a number
+number = int(input("Enter a number: "))
+
+#ask user specified length
+specified_length = int(input("Enter a number for specified length: "))
+
+#find total space needed to add zeros
+#if statement using rjust function and else statement
+#prints the complete number of characters
