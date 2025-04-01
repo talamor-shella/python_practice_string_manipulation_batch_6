0ca8483014a6d0205d387e922da1fea741c47c60
talamor-shella <shellatalamor@gmail.com>
2025-04-01 18:24:12 +0800

    addedpseudocode

--- create_a_program_that_do_the_same_functionality_without_using_rindex_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_rindex_function.py
@@ -1,2 +1,7 @@
 #Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
+#ask user input a string
+#ask user input what they want to find the index
+#for loop through string from the last index
+#if statement
+#prints the index 
