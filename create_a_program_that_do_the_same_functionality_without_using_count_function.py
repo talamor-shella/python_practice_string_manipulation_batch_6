aff804e8f94440eaa40b929d1379df023b0dc3a9
talamor-shella <shellatalamor@gmail.com>
2025-04-01 15:50:52 +0800

    addedpseudocode

--- create_a_program_that_do_the_same_functionality_without_using_count_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_count_function.py
@@ -1,1 +1,8 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+#ask user input word or sentence
+#ask user input a character they want to count
+#initialize a counter
+#for loop
+#if-else statement
+#prints the number of occurence 
