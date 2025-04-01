f8c3d15ee5c334047251d66ff320bfb35682ba34
talamor-shella <shellatalamor@gmail.com>
2025-04-01 18:25:33 +0800

    ask user input a string and what they want to find index

--- create_a_program_that_do_the_same_functionality_without_using_rindex_function.py
+++ create_a_program_that_do_the_same_functionality_without_using_rindex_function.py
@@ -1,7 +1,11 @@
 #Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
 #ask user input a string
+string = input("Enter a string: ")
+
 #ask user input what they want to find the index
+character = input("Enter a character you want to find the index: ")
+
 #for loop through string from the last index
 #if statement
 #prints the index 
